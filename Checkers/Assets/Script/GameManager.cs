d3f385b4994df3220abcfc0faa4d72b648aac75f
Christopher Weir <christopher.weir@hotmail.com>
2017-03-04 18:37:07 +0000

    Created a working connection between the Server and Client

--- Checkers/Assets/Script/GameManager.cs
+++ Checkers/Assets/Script/GameManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class GameManager : MonoBehaviour
     public GameObject serverMenu;
     public GameObject connectMenu;
 
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
+
+    public InputField nameInput;
 
     private void Start()
     {
@@ -27,13 +33,44 @@ public class GameManager : MonoBehaviour
 
     public void HostButton()
     {
+        try
+        {
+            Server s = Instantiate(serverPrefab).GetComponent<Server>();
+            s.Init();
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            if (c.clientName == "")
+                c.clientName = "Host";
+            c.ConnectToServer("127.0.0.1", 2017);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
         mainMenu.SetActive(false);
         serverMenu.SetActive(true);
     }
 
     public void ConnectToServerButton()
     {
-        
+        string hostAddress = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (hostAddress == "")
+            hostAddress = "127.0.0.1";
+
+        try
+        {
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            if (c.clientName == "")
+                c.clientName = "Client";
+            c.ConnectToServer(hostAddress, 2017);
+            connectMenu.SetActive(false);
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
     }
 
     public void BackButton()
@@ -41,5 +78,13 @@ public class GameManager : MonoBehaviour
         serverMenu.SetActive(false);
         connectMenu.SetActive(false);
         mainMenu.SetActive(true);
+
+        Server s = FindObjectOfType<Server>();
+        if (s != null)
+            Destroy(s.gameObject);
+
+        Client c = FindObjectOfType<Client>();
+        if (c != null)
+            Destroy(c.gameObject);
     }
 }
