b3e4b71863163a63e1dcc8951d81c39e4422fc4f
Christopher Weir <christopher.weir@hotmail.com>
2017-03-30 17:30:08 +0100

    updates

---
+++ Checkers 2.0/Builds/Checkers 2.0/SpeechHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace Checkers_2._0
+{
+    public class SpeechHelper
+    {
+        public static void HandleSpeechCommand(IActivatedEventArgs args)
+        {
+            var commandArgs = args as
+          Windows.ApplicationModel.Activation.VoiceCommandActivatedEventArgs;
+            Windows.Media.SpeechRecognition.SpeechRecognitionResult
+          speechRecognitionResult = commandArgs.Result;
+
+            string textSpoken = speechRecognitionResult.Text;
+
+            CortanaInterop.CortanaText = textSpoken;
+        }
+    }
+}
